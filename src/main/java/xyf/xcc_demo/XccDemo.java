a8a2e5e77bfd9874927e91ded7c57f54ee04366d
xuyf <124990301@qq.com>
2019-11-21 21:33:22 +0800

    修改

--- src/main/java/xyf/xcc_demo/XccDemo.java
+++ src/main/java/xyf/xcc_demo/XccDemo.java
@@ -18,10 +18,10 @@ public class XccDemo {
          * threadFactory - 执行程序创建新线程时使用的工厂
          * handler - 执行被阻止时使用的处理程序，因为达到线程限制和队列容量
          *
-         * 创建一个线程池，正常情况有5个线程，最大10个（5个闲置销毁的线程），闲置销毁时间5分钟，任务队列可以存放100个任务，用默认的线程工厂创建线程，当线程超过10个（maximumPoolSize），再需要创建新线程时，会抛异常
+         * 创建一个线程池，正常情况有5个线程，最大10个（5个闲置销毁的线程），闲置销毁时间5分钟，任务队列可以存放9个任务，用默认的线程工厂创建线程，当线程超过10个（maximumPoolSize），再需要创建新线程时，会抛异常
          * 当同一时间任务数大于maximumPoolSize+队列允许的最大线程数之和时，就会报错
          */
-        ExecutorService pool = new ThreadPoolExecutor(5,10,5L, TimeUnit.MINUTES,new ArrayBlockingQueue(100),Executors.defaultThreadFactory(),new ThreadPoolExecutor.AbortPolicy() );
+        ExecutorService pool = new ThreadPoolExecutor(5,10,5L, TimeUnit.MINUTES,new ArrayBlockingQueue(9),Executors.defaultThreadFactory(),new ThreadPoolExecutor.AbortPolicy() );
         //固定大小的线程池,默认的任务队列无限大，因此maximumPoolSize永远等不到新建线程，所以没有用
         ExecutorService pool2 = Executors.newFixedThreadPool(5);
         //无线容量线程池，所有任务都会临时创建线程，并在默认的时间后销毁
@@ -31,16 +31,16 @@ public class XccDemo {
             @Override
             public void run() {
                 System.out.println("id:"+Thread.currentThread().getId()+"    name:"+Thread.currentThread().getName());
-//                try {
-//                    Thread.sleep(3000L);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
+                try {
+                    Thread.sleep(3000L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         };
         //将任务交给线程池对象，线程池对象会取出线程执行任务
         for(int i=0;i<20;i++){
-            pool3.execute(runnable);
+            pool.execute(runnable);
         }
 
     }
